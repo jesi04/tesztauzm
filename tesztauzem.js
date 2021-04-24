15ddb88ac6f98b7f5416ade1d9b9b561ddadb8ea
jesi04 <71004551+jesi04@users.noreply.github.com>
2021-04-24 20:24:09 +0200

    Add files via upload

---
+++ tesztauzem.js
@@ -0,0 +1,22 @@
+﻿
+$(document).ready(function() {
+    $.ajax({
+        "url": "/api/akcio", 
+        "method": "GET",
+    }).done(function(msg) {
+        $("#akcios-teszta").html(msg.akciosTeszta);
+        $("#akcios-ar").html(msg.akciosAr);
+    });
+});
+
+$("button").click(function() {
+    $.ajax({
+        "url": "/api/jatek", 
+        "method": "POST",
+        "data": {
+            "uzenet": $("#jatek-valasz").val()
+        }
+    }).done(function(msg) {
+        alert("Köszönjük, hogy részt vett játékunkban");
+    });
+});
